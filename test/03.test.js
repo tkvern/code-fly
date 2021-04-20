1c16d438d84ee4ad7add3bbd8878a5e8e9d666d9
tkvern <tkvern@qq.com>
2021-04-20 21:10:42 +0800

    feat: 移动位置

--- test/03.test.js
+++ test/03.test.js
@@ -1,4 +1,4 @@
-const duplicate = require('../03')
+const duplicate = require('../src/03')
 
 test('03 数组中重复的数字 - 有重复', () => {
   const params = [2, 3, 1, 0, 2, 5]
--- test/04.test.js
+++ test/04.test.js
@@ -1,4 +1,4 @@
-const duplicate = require('../04')
+const duplicate = require('../src/04')
 
 test('04 二维数组中的查找', () => {
   const params = [2, 3, 1, 0, 2, 5]
