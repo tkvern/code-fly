9d9e745470a9ab6e29a3c72c23dc963ca2c1245a
tkvern <tkvern@qq.com>
2021-04-20 21:08:47 +0800

    feat: 二维数组中的查找

--- 04.js
+++ 04.js
@@ -8,4 +8,20 @@ function find(target, matrix) {
   if (matrix === null || matrix.length === 0 || matrix[0].length === 0) {
     return false
   }
+  let rows = matrix.length
+  let cols = matrix[0].length
+  let r = 0
+  let c = cols - 1
+  while (r <= rows - 1 && c >= 0) {
+    if (target == matrix[r][c]) {
+      return true
+    } else if (target > matrix[r][c]) {
+      r++
+    } else {
+      c--
+    }
+  }
+  return false
 }
+
+module.exports = find
