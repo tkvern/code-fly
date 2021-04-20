cab9fd6e37ff60148c0d2603118605c3bdace14e
tkvern <tkvern@qq.com>
2021-04-20 20:24:49 +0800

    feat: 增加preitter

--- test/04.test.js
+++ test/04.test.js
@@ -1,11 +1,11 @@
-const duplicate = require('../03')
+const duplicate = require('../04')
 
-test('03 数组中重复的数字 - 有重复', () => {
+test('04 二维数组中的查找', () => {
   const params = [2, 3, 1, 0, 2, 5]
   expect(duplicate(params)).toBe(2)
 })
 
-test('03 数组中重复的数字 - 没有重复', () => {
+test('04 二维数组中的查找', () => {
   const params = [4, 3, 1, 0, 2, 5]
   expect(duplicate(params)).toBe(-1)
 })
