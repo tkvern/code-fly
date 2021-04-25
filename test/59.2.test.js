005ab153e1d53425074f021f6821e0f448e038ca
tkvern <tkvern@qq.com>
2021-04-25 17:31:32 +0800

    feat: 增加59.2 队列的最大值

---
+++ test/59.2.test.js
@@ -0,0 +1,16 @@
+let MaxQueue = require('../src/59.2')
+
+test('59.2 队列的最大值 用例1', () => {
+  let maxQueue = new MaxQueue()
+  maxQueue.pushBack(1)
+  maxQueue.pushBack(2)
+  expect(maxQueue.getMaxValue()).toBe(2)
+  expect(maxQueue.popFront()).toBe(1)
+  expect(maxQueue.getMaxValue()).toBe(2)
+})
+
+test('59.2 队列的最大值 用例2', () => {
+  let maxQueue = new MaxQueue()
+  expect(maxQueue.popFront()).toBe(-1)
+  expect(maxQueue.getMaxValue()).toBe(-1)
+})
