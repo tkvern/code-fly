11bd7a25ea49a055c1493d75534f8b8c708996eb
tkvern <tkvern@qq.com>
2021-04-26 17:57:01 +0800

    test: 增加 57.1 的测试用例

--- test/57.1.test.js
+++ test/57.1.test.js
@@ -8,6 +8,12 @@ test('57.1 和为 s 的两个数字 - 用例1', () => {
 
 test('57.1 和为 s 的两个数字 - 用例2', () => {
   const nums = [10, 26, 30, 31, 47, 60]
-  const target = 40
+  const target = 50
   expect(toSum(nums, target)).toEqual([10, 30])
+})
+
+test('57.1 和为 s 的两个数字 - 用例3', () => {
+  const nums = [10, 26, 30, 31, 47, 60]
+  const target = 86
+  expect(toSum(nums, target)).toEqual([26, 60])
 })
