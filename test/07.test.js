f7d01729be5d86c437019e9875460212e4be4ac0
tkvern <tkvern@qq.com>
2021-05-05 17:03:25 +0800

    feat: 增加题目07 重建二叉树

---
+++ test/07.test.js
@@ -0,0 +1,7 @@
+let buildTree = require('../src/07')
+
+test('07. 重建二叉树', () => {
+  const preorder = [3, 9, 20, 15, 7]
+  const inorder = [9, 3, 15, 20, 7]
+  expect(buildTree(preorder, inorder).val).toEqual(3)
+})
