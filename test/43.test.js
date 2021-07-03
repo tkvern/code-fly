aa8d565048bc93a926fdbdc39eb0a5b2e11b3d0f
tkvern <tkvern@qq.com>
2021-07-03 15:48:49 +0800

    chore: 完善测试用例描述

--- test/43.test.js
+++ test/43.test.js
@@ -1,6 +1,11 @@
 const countDigitOne = require('../src/43')
 
-test('43 1 ～ n 整数中 1 出现的次数', () => {
+test('43 1 ～ n 整数中 1 出现的次数 - 用例1', () => {
   const n = 13
   expect(countDigitOne(n)).toBe(6)
+})
+
+test('43 1 ～ n 整数中 1 出现的次数 - 用例2', () => {
+  const n = 0
+  expect(countDigitOne(n)).toBe(0)
 })
